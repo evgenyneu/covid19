6ae2e7eb0e1231c930050cc9f52b06a5170bf59f
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-03-10 11:58:24 +1100

    Work

--- code/run_model.py
+++ code/run_model.py
@@ -1,25 +1,11 @@
 import pandas as pd
 from datetime import datetime
 from cmdstanpy import CmdStanModel
-import matplotlib.pyplot as plt
-from tabulate import tabulate
-import scipy.stats as stats
-import numpy as np
 from dataclasses import dataclass
 from tarpan.cmdstanpy.analyse import save_analysis
-from tarpan.cmdstanpy.tree_plot import save_tree_plot
 from tarpan.cmdstanpy.cache import run
-from tarpan.cmdstanpy.compare_parameters import save_compare_parameters
-from tarpan.shared.compare_parameters import CompareParametersType
-from tarpan.shared.info_path import InfoPath, get_info_path
-from tarpan.shared.tree_plot import TreePlotParams
+from tarpan.shared.info_path import InfoPath
 from tarpan.shared.summary import save_summary
-from tarpan.shared.histogram import save_histogram
-from tarpan.shared.tree_plot import save_tree_plot as shared_save_tree_plot
-from tarpan.plot.kde import save_scatter_and_kde
-
-from tarpan.plot.posterior import (
-    save_posterior_scatter_and_kde, PosteriorKdeParams)
 
 
 # Parameters for data analysys
@@ -28,7 +14,7 @@ class AnalysisSettings:
     # Name of the parent directory where the plots will be created.
     dir_name: str = "model_info"
 
-    # Data for Stan model
+    # Data for Stan model (dictionary)
     data = None
 
     # Path to the .stan model file
@@ -39,6 +25,10 @@ class AnalysisSettings:
 
     # Location of plots and summaries
     info_path: InfoPath = None
+
+    population_size: float = 7800000
+
+    q: float = None
 
 
 def load_data(data_path):
@@ -70,49 +60,20 @@ def load_data(data_path):
         date = datetime.strptime(column_name, '%m/%d/%y')
         dates.append(date)
         confirmed = df[column_name].sum()
-        cases.append(confirmed)
+        cases.append(int(confirmed))
 
     return dates, cases
 
 
-def standardise(measurements):
-    """
-    Converts an array to standardised values
-    (i.e. z-scores, values minus their means divided by standard deviation)
-
-    Parameters
-    ----------
-
-    measurements: list of float
-        Values to standardise
-
-    Returns
-    --------
-    np.array:
-        Standardised values.
-    """
-    measurements = np.array(measurements)
-    mean = measurements.mean()
-    std = measurements.std()
-
-    return (measurements - mean) / std
-
-
-def standardised_data_for_stan(measurements, uncertainties,
-                               sigma_stdev_prior):
+def data_for_stan(cases, settings):
     """
     Returns data for the model.
 
     Parameters
     ----------
 
-    measurements: list of float
-        Measurements.
-    uncertainties: list of float
-        Measurement uncertainties.
-    sigma_stdev_prior: float
-        Prior for standard deviation of the population spread,
-        in measurement units (i.e. non-stsndardised).
+    list of float:
+        Number of people infected (confirmed).
 
     Returns
     -------
@@ -121,60 +82,15 @@ def standardised_data_for_stan(measurements, uncertainties,
         Data that is supplied to Stan model.
     """
 
-    measurements = np.array(measurements)
-    std = measurements.std()
-    measurements_std = standardise(measurements=measurements)
-    uncertainties_std = uncertainties / std
-    sigma_stdev_prior_std = sigma_stdev_prior / std
+    q = -1 + settings.population_size / cases[0]
+    print(q)
 
     return {
-        "y": measurements_std,
-        "uncertainties": uncertainties_std,
-        "n": len(measurements_std),
-        "sigma_stdev_prior": sigma_stdev_prior_std
+        "n": len(cases),
+        "cases": cases,
+        "k": settings.population_size,
+        "q": q
     }
-
-
-def destandardise_stan_output(fit, measurements):
-    """
-    Transform Stan output values from standardised form to the scale
-    of observations.
-
-    Parameters
-    ----------
-
-    fit: cmdstanpy.stanfit.CmdStanMCMC
-        Stan'd output to standardise. The function transforms `fit.sample`
-        array from standard form.
-
-    measurements: list of float
-        Measurements, non-standardised.
-    """
-    measurements = np.array(measurements)
-    mean = measurements.mean()
-    std = measurements.std()
-
-    # Destandardise 'mu' parameters: 'mu.1', 'mu.2' etc.
-    # -----------
-
-    mu_columns = [name for name in fit.column_names if name.startswith('mu')]
-
-    for column_name in mu_columns:
-        column_id = fit.column_names.index(column_name)
-
-        for chain_id in range(fit.chains):
-            values = fit.sample[:, chain_id, column_id]
-            fit.sample[:, chain_id, column_id] = values * std + mean
-
-
-    # De-standardise `sigma`
-    # ----------
-
-    column_id = fit.column_names.index('sigma')
-
-    for chain_id in range(fit.chains):
-        values = fit.sample[:, chain_id, column_id]
-        fit.sample[:, chain_id, column_id] = values * std
 
 
 def run_stan(output_dir, settings: AnalysisSettings):
@@ -206,8 +122,7 @@ def run_stan(output_dir, settings: AnalysisSettings):
         output_dir=output_dir)
 
     # Make summaries and plots of parameter distributions
-    save_analysis(fit, param_names=["r", "mu", "sigma"],
-                  info_path=settings.info_path)
+    save_analysis(fit, param_names=["b", "sigma"])
 
     return fit
 
@@ -236,16 +151,16 @@ def check_all_days_present(dates):
 def run_model(settings):
     dates, cases = load_data("data/time_series_19-covid-Confirmed.csv")
     check_all_days_present(dates)
+    settings.data = data_for_stan(cases, settings=settings)
 
     # plt.scatter(dates, cases)
     # plt.savefig("confirmed.png", dpi=300)
-    # fit = run(func=run_stan, settings=settings)
-
-    # return fit
+    fit = run(func=run_stan, settings=settings)
 
 
 if __name__ == '__main__':
     print("Running the model...")
-    settings = AnalysisSettings(stan_model_path="code/stan_model/model.stan")
+    model_path = "code/stan_model/logistic.stan"
+    settings = AnalysisSettings(stan_model_path=model_path)
     run_model(settings=settings)
     print('We are done')
---
+++ code/stan_model/logistic.stan
@@ -0,0 +1,25 @@
+data {
+  int n;           // Number of data point
+  real cases[n];   // Number of infected people at successive days
+  real k;          // Population size
+
+  // Logistic function parameter related to initial number of infected people
+  real q;
+}
+
+parameters {
+  real<lower=0> b;      // Growth rate
+  real<lower=1> sigma;  //
+}
+
+model {
+  vector[n] mu;
+  sigma ~ exponential(1);
+  b ~ exponential(1);
+
+  for (day in 1:n) {
+      mu[day] = k / (1 + q * exp(-(b * (day - 1))) );
+  }
+
+  cases ~ normal(mu, sigma);
+}
